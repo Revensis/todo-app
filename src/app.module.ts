9c703b93ff091199697d7ee7514126e48f095076
Sofiane Amenzou <sofiane.amenzou@atos.net>
2025-03-12 18:31:00 +0100

    feat: enable cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { TasksModule } from './tasks/tasks.module';
+import { TasksModule } from './Tasks/tasks.module';
 
 @Module({
   imports: [
